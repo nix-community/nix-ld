2e34ced98a83bacaf22cfec6254dbfcb092c5527
Jörg Thalheim <joerg@thalheim.io>
2020-11-15 12:33:36 +0100

    add master-pdf-editor example

---
+++ examples/masterpdfeditor.nix
@@ -0,0 +1,24 @@
+with import <nixpkgs> {};
+
+mkShell {
+  NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
+    nss
+    sane-backends
+    nspr
+    qt5.qtbase
+    qt5.qtsvg
+    stdenv.cc.cc
+  ];
+  QT_PLUGIN_PATH = "${qt5.qtbase}/${qt5.qtbase.qtPluginPrefix}";
+  shellHook = ''
+    if [ ! -d master-pdf-editor ]; then
+      echo "unpack master-pdf-editor..."
+      mkdir master-pdf-editor
+      tar -C master-pdf-editor \
+          --strip-components 1 \
+          -xf ${masterpdfeditor.src}
+    fi
+    echo '$ ./master-pdf-editor/masterpdfeditor5'
+    ./master-pdf-editor/masterpdfeditor5
+  '';
+}
