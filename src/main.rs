659622a9a09bf0debfc284de0205e685c2ab1d4d
Jörg Thalheim <joerg@thalheim.io>
2020-11-22 18:00:56 +0100

    move environment processing into function

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,9 @@ mod start;
 mod syscall;
 mod unwind_resume;
 use core::fmt::Write;
+use core::slice::from_raw_parts as mkslice;
 
-use crate::print::{PrintBuffer, print};
+use crate::print::PrintBuffer;
 pub use crate::start::_start;
 
 #[lang = "eh_personality"]
@@ -42,29 +43,41 @@ pub unsafe fn strlen(mut s: *const u8) -> usize {
     count
 }
 
-fn starts_with<T>(slice: &[T], prefix: &[T]) -> bool
-where
-    T: PartialEq,
-{
-    if slice.len() < prefix.len() {
-        false
-    } else {
-        // this is not an idiomatic for loop - but the for..in
-        // version using ranges *also* pulls in compiler builtins
-        // we don't currently have.
-        let mut i = 0;
-        while i < prefix.len() {
-            if slice[i] != prefix[i] {
-                return false;
-            }
-            i += 1;
-        }
-        true
-    }
+const NIX_LD : &'static str = "NIX_LD=";
+const NIX_LD_LIB_PATH : &'static str = "NIX_LD_LIBRARY_PATH=";
+
+struct LdConfig {
+    exe: Option<&'static str>,
+    lib_path: Option<&'static str>,
 }
 
-const NIX_LD : &[u8; 7] = b"NIX_LD=";
-const NIX_LD_LIB_PATH : &[u8; 20] = b"NIX_LD_LIBRARY_PATH=";
+unsafe fn str_slice_from_ptr(ptr: *const u8) -> &'static str {
+    str::from_utf8_unchecked(mkslice(ptr, strlen(ptr)))
+}
+
+unsafe fn process_env(mut envp: *const *const u8) -> LdConfig {
+    let mut config = LdConfig { exe: None, lib_path: None };
+    while !(*envp).is_null() {
+        let var = str_slice_from_ptr(*envp);
+        if var.starts_with(NIX_LD) {
+            config.exe = Some(&var[NIX_LD.len()..]);
+        };
+        if var.starts_with(NIX_LD_LIB_PATH) {
+            config.lib_path = Some(&var[NIX_LD_LIB_PATH.len()..]);
+        };
+
+        envp = envp.add(1);
+    }
+    config
+}
+
+unsafe fn exe_name(args: &[*const u8]) -> &str {
+    if args.len() > 0 {
+        str_slice_from_ptr(args[0])
+    } else {
+        ""
+    }
+}
 
 /// # Safety
 ///
@@ -73,42 +86,22 @@ const NIX_LD_LIB_PATH : &[u8; 20] = b"NIX_LD_LIBRARY_PATH=";
 pub unsafe fn main(stack_top: *const u8) {
     let argc = *(stack_top as *const isize);
     let argv = stack_top.add(size_of::<*const isize>()) as *const *const u8;
-    let mut envp = argv.add(argc as usize + 1) as *const *const u8;
+    let envp = argv.add(argc as usize + 1) as *const *const u8;
 
-    use core::slice::from_raw_parts as mkslice;
     let args = mkslice(argv, argc as usize);
+
+    let ld_config = process_env(envp);
 
     let mut buf = [0u8; 4096];
     let mut buf = PrintBuffer::new(&mut buf[..]);
 
-    for &arg in args {
-        let arg = mkslice(arg, strlen(arg));
-        print(arg);
+    if ld_config.exe.is_none() {
+        eprint!(buf, "Cannot execute binary {}: No NIX_LD environment variable set", exe_name(args));
     }
 
-    let mut nix_ld: Option<&[u8]> = None;
-    let mut nix_ld_lib_path: Option<&[u8]> = None;
-
-    while !(*envp).is_null() {
-        let var = *envp;
-        let var = mkslice(var, strlen(var));
-        if starts_with(var, NIX_LD) {
-            nix_ld = Some(&var[NIX_LD.len()..]);
-        };
-        if starts_with(var, NIX_LD_LIB_PATH) {
-            nix_ld_lib_path = Some(&var[NIX_LD_LIB_PATH.len()..]);
-        };
-
-        envp = envp.add(1);
+    if let Some(lib_path) = ld_config.lib_path {
+        eprint!(buf, "ld_library_path: {}\n", lib_path);
     }
 
-    if let Some(ld) = nix_ld {
-        print!(buf, "ld_path {}\n", str::from_utf8_unchecked(ld));
-    }
-
-    if let Some(ld_lib_path) = nix_ld_lib_path {
-        eprint!(buf, "ld_library_path: {}\n", str::from_utf8_unchecked(ld_lib_path));
-    }
-
-    syscall::exit(argc as i32 - 1);
+    syscall::exit(0);
 }
--- src/string.rs
+++ src/string.rs
@@ -1,21 +1,56 @@
+use libc::{c_void, c_int, size_t};
+use core::mem;
+
 #[no_mangle]
-pub unsafe extern "C" fn memcpy(s1: *mut u8, s2: *const u8, n: usize) -> *mut u8 {
+pub unsafe extern "C" fn memcpy(s1: *mut c_void, s2: *const c_void, n: size_t) -> *mut c_void {
     let mut i = 0;
     while i + 7 < n {
         *(s1.add(i) as *mut u64) = *(s2.add(i) as *const u64);
         i += 8;
     }
     while i < n {
-        *s1.add(i) = *s2.add(i);
+        *(s1 as *mut u8).add(i) = *(s2 as *const u8).add(i);
         i += 1;
     }
     s1
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn memset(s: *mut u8, c: isize, n: usize) -> *mut u8 {
+pub unsafe extern "C" fn memset(s: *mut c_void, c: c_int, n: size_t) -> *mut c_void {
     for i in 0..n {
         *(s as *mut u8).add(i) = c as u8;
     }
     s
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn memcmp(s1: *const c_void, s2: *const c_void, n: size_t) -> c_int {
+    let (div, rem) = (n / mem::size_of::<usize>(), n % mem::size_of::<usize>());
+    let mut a = s1 as *const usize;
+    let mut b = s2 as *const usize;
+    for _ in 0..div {
+        if *a != *b {
+            for i in 0..mem::size_of::<usize>() {
+                let c = *(a as *const u8).add(i);
+                let d = *(b as *const u8).add(i);
+                if c != d {
+                    return c as c_int - d as c_int;
+                }
+            }
+            unreachable!()
+        }
+        a = a.offset(1);
+        b = b.offset(1);
+    }
+
+    let mut a = a as *const u8;
+    let mut b = b as *const u8;
+    for _ in 0..rem {
+        if *a != *b {
+            return *a as c_int - *b as c_int;
+        }
+        a = a.offset(1);
+        b = b.offset(1);
+    }
+    0
 }
