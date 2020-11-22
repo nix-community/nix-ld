4c82ff3911d08cf921a8f33001c846436ec51759
Jörg Thalheim <joerg@thalheim.io>
2020-11-22 11:46:13 +0100

    unwind_resume: print error message on unwind_resume

--- src/print.rs
+++ src/print.rs
@@ -1,6 +1,5 @@
 use crate::syscall::write;
-
-pub const STDOUT_FILENO: u32 = 1;
+use libc::{STDOUT_FILENO, STDERR_FILENO};
 
 pub fn print(s: &[u8]) {
     unsafe {
@@ -8,7 +7,18 @@ pub fn print(s: &[u8]) {
     }
 }
 
+pub fn eprint(s: &[u8]) {
+    unsafe {
+        write(STDERR_FILENO as i32, s.as_ptr() as *const libc::c_void, s.len());
+    }
+}
+
 pub fn println(s: &[u8]) {
     print(s);
     print(b"\n");
+}
+
+pub fn eprintln(s: &[u8]) {
+    eprint(s);
+    eprint(b"\n");
 }
--- src/unwind_resume.rs
+++ src/unwind_resume.rs
@@ -1,6 +1,8 @@
 use crate::syscall;
+use crate::print::eprintln;
 
 #[no_mangle]
 pub unsafe extern "C" fn _Unwind_Resume() {
+    eprintln(b"_Unwind_Resume");
     syscall::exit(1);
 }
