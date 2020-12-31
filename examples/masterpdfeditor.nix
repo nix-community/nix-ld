d90dccae115a5131e369edb3f85ab042a65ee19f
Jörg Thalheim <joerg@thalheim.io>
2020-12-31 11:30:07 +0100

    nix-ld: strip trailing newline in dynamic linker

--- examples/masterpdfeditor.nix
+++ examples/masterpdfeditor.nix
@@ -9,10 +9,12 @@ mkShell {
     qt5.qtsvg
     stdenv.cc.cc
   ];
-  QT_PLUGIN_PATH = "${qt5.qtbase}/${qt5.qtbase.qtPluginPrefix}";
-  shellHook = ''
-    export NIX_LD="$(cat $NIX_CC/nix-support/dynamic-linker)"
 
+  NIX_LD = builtins.readFile "${stdenv.cc}/nix-support/dynamic-linker";
+
+  QT_PLUGIN_PATH = "${qt5.qtbase}/${qt5.qtbase.qtPluginPrefix}";
+
+  shellHook = ''
     if [ ! -d master-pdf-editor ]; then
       echo "unpack master-pdf-editor..."
       mkdir master-pdf-editor
