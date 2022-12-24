e41f97ced5d85294c0150ee532704f6f6c468dc8
Jörg Thalheim <joerg@thalheim.io>
2022-12-24 08:04:35 +0100

    fix 22.11 usage

--- modules/nix-ld.nix
+++ modules/nix-ld.nix
@@ -1,7 +1,9 @@
-{pkgs, ...}:
+{pkgs, lib, ...}:
 {
   config = {
     programs.nix-ld.enable = true;
+  } // lib.optionalAttrs (lib.versionAtLeast (lib.versions.majorMinor lib.version) "23.05") {
+    # 22.11 users won't actually use nix-ld from this repo but at least it does not break their configuration.
     programs.nix-ld.package = pkgs.callPackage ../nix-ld.nix {};
   };
 }
