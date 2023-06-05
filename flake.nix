e70b775eb4f83f4a7b4979f8d292997c3263a1da
Jörg Thalheim <joerg@thalheim.io>
2023-06-05 10:05:39 +0200

    drop flake-utils

--- flake.nix
+++ flake.nix
@@ -1,25 +1,23 @@
 {
   description = "nix-ld: run unpatched dynamic binaries on NixOS";
 
-  inputs.utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
   nixConfig.extra-substituters = [ "https://cache.garnix.io" ];
   nixConfig.extra-trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
 
-  outputs = {
-    self,
-    nixpkgs,
-    utils,
-  }:
-    {
-      nixosModules.nix-ld = import ./modules/nix-ld.nix;
-    }
-    // utils.lib.eachSystem ["x86_64-linux" "aarch64-linux"] (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      packages.nix-ld = pkgs.callPackage ./default.nix {};
-      packages.default = self.packages.${system}.nix-ld;
-      checks = self.packages.${system}.nix-ld.tests;
-    });
+  outputs = { self, nixpkgs }: {
+    nixosModules.nix-ld = import ./modules/nix-ld.nix;
+    packages = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        nix-ld = pkgs.callPackage ./default.nix { };
+        default = self.packages.${system}.nix-ld;
+      });
+    checks = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (system:
+      self.packages.${system}.nix-ld.tests
+    );
+  };
 }
