716a280a3af9528c8a424a142caeb1f0e40b9020
Zhaofeng Li <hello@zhaofeng.li>
2023-06-20 19:44:20 -0600

    build.rs: Why did I do that?

--- build.rs
+++ build.rs
@@ -41,7 +41,7 @@ fn main() {
         println!("cargo:rustc-env=DEFAULT_NIX_LD={}", target_default_nix_ld);
     }
 
-    if let Ok(nix_system) = env::var(format!("NIX_SYSTEM")) {
+    if let Ok(nix_system) = env::var("NIX_SYSTEM") {
         let underscored = nix_system.replace('-', "_");
         println!("cargo:rustc-env=NIX_SYSTEM={}", underscored);
     }
