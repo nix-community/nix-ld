86cb3d925b277db98b07295ad457c69462180a3a
Jörg Thalheim <joerg@thalheim.io>
2024-12-02 07:51:31 +0100

    fix masterpdfeditor example

--- examples/masterpdfeditor.nix
+++ examples/masterpdfeditor.nix
@@ -1,34 +1,40 @@
-with import <nixpkgs> {};
+{
+  pkgs ? import <nixpkgs> { },
+}:
+let
+  inherit (pkgs) lib stdenv qt5;
+in
 # run with
 # $ nix-shell ./masterpdfeditor.nix
-  mkShell {
-    NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
-      nss
-      sane-backends
-      nspr
-      zlib
-      libglvnd
-      qt5.qtbase
-      qt5.qtsvg
-      qt5.qtdeclarative
-      qt5.qtwayland
-      stdenv.cc.cc
-    ];
+pkgs.mkShell {
+  NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
+    pkgs.nss
+    pkgs.sane-backends
+    pkgs.nspr
+    pkgs.zlib
+    pkgs.libglvnd
+    qt5.qtbase
+    qt5.qtsvg
+    qt5.qtdeclarative
+    qt5.qtwayland
+    pkgs.pkcs11helper
+    stdenv.cc.cc
+  ];
 
-    NIX_LD = builtins.readFile "${stdenv.cc}/nix-support/dynamic-linker";
+  NIX_LD = builtins.readFile "${stdenv.cc}/nix-support/dynamic-linker";
 
-    QT_PLUGIN_PATH = "${qt5.qtbase}/${qt5.qtbase.qtPluginPrefix}:${qt5.qtwayland.bin}/${qt5.qtbase.qtPluginPrefix}";
-    QML2_IMPORT_PATH = "${qt5.qtdeclarative.bin}/${qt5.qtbase.qtQmlPrefix}:${qt5.qtwayland.bin}/${qt5.qtbase.qtQmlPrefix}";
+  QT_PLUGIN_PATH = "${qt5.qtbase}/${qt5.qtbase.qtPluginPrefix}:${qt5.qtwayland.bin}/${qt5.qtbase.qtPluginPrefix}";
+  QML2_IMPORT_PATH = "${qt5.qtdeclarative.bin}/${qt5.qtbase.qtQmlPrefix}:${qt5.qtwayland.bin}/${qt5.qtbase.qtQmlPrefix}";
 
-    shellHook = ''
-      if [ ! -d master-pdf-editor ]; then
-        echo "unpack master-pdf-editor..."
-        mkdir master-pdf-editor
-        tar -C master-pdf-editor \
-            --strip-components 1 \
-            -xf ${masterpdfeditor.src}
-      fi
-      echo '$ ./master-pdf-editor/masterpdfeditor5'
-      ./master-pdf-editor/masterpdfeditor5
-    '';
-  }
+  shellHook = ''
+    if [ ! -d master-pdf-editor ]; then
+      echo "unpack master-pdf-editor..."
+      mkdir master-pdf-editor
+      tar -C master-pdf-editor \
+          --strip-components 1 \
+          -xf ${pkgs.masterpdfeditor.src}
+    fi
+    echo '$ ./master-pdf-editor/masterpdfeditor5'
+    ./master-pdf-editor/masterpdfeditor5
+  '';
+}
